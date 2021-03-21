5ba68fa55ab848b96a18a32a282586cc2be00d12
Jack <jhburrus3@gmail.com>
2021-03-21 18:48:15 -0500

    Able to connect to iota

--- src/components/SendForm/SendForm.tsx
+++ src/components/SendForm/SendForm.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { Button, TextInput } from "react-native";
 
 import Box from "../../theme/Box";
 import Text from "../../theme/Text";
 
+const Iota = require("@iota/core");
+
 interface SendFormProps {}
 
 export const SendForm = (props: SendFormProps) => {
   const { control, handleSubmit, errors } = useForm();
   const onSubmit = (data) => console.log(data);
+
+  //   useEffect(() => {
+  //     const iota = Iota.composeAPI({
+  //       provider: "https://nodes.comnet.thetangle.org:443",
+  //     });
+  //     iota
+  //       .getNodeInfo()
+  //       // Convert the returned object to JSON to make the output more readable
+  //       .then((info) => console.log(JSON.stringify(info, null, 1)))
+  //       .catch((err) => {
+  //         // Catch any errors
+  //         console.log(err);
+  //       });
+  //   }, []);
 
   return (
     <Box flex={1}>
@@ -18,13 +34,16 @@ export const SendForm = (props: SendFormProps) => {
         render={({ onChange, onBlur, value }) => (
           <TextInput
             style={{
-              color: "grey",
+              color: "white",
+              fontFamily: "Hind_400Regular",
               borderWidth: 1,
               height: 50,
               marginLeft: 10,
               marginRight: 10,
               borderRadius: 5,
+              paddingLeft: 10,
             }}
+            // placeholderTextColor={"white"}
             onBlur={onBlur}
             onChangeText={(value) => onChange(value)}
             defaultValue={"Address"}
